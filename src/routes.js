c5486f370e399857271177e2d3f06517fe7370b4
Krissy Hales <kristinakfriel@gmail.com>
2019-07-02 15:16:58 -0600

    changes

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import About from './components/About/About'
+import Home from './components/Home/Home';
+import ClassList from './components/ClassList/ClassList';
+import Student from './components/Student/Student';
+
+export default (
+    <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/about' component={About}/>
+        <Route path='/classList/:class' component={ClassList}/>
+        <Route path='/student/:id' component={Student} />
+    </Switch>
+)
